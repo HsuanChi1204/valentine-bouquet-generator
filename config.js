0f76feaca16be5be21dfe14c751e626494bd5d64
HsuanChi1204 <kevin851204@gmail.com>
2025-02-14 11:35:10 -0500

    Add API key directly in config.js and remove API key input interface

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    OPENAI_API_KEY: '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'
+};
+
+export default config; 
